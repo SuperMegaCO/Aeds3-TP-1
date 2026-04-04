89e2dce5f5f8e7b794c6acc70c87bc0bb5acc9a4
Gabriel <gabriel.couto@sga.pucminas.br>
2026-04-04 12:10:07 -0300

    Mudanca de idCurso para NomeCurso na arvore B+

--- ArvoreBMais/Main.java
+++ ArvoreBMais/Main.java
@@ -1,16 +1,3 @@
-/*
-TESTE DE ÁRVORE B+
-
-Este programa principal serve para demonstrar o uso
-da Árvore B+ como um relacionamento entre duas entidades.
-
-Aqui, cada elemento será composto por dois inteiros que 
-representam IDs de entidades quaisquer.
-
-Implementado pelo Prof. Marcos Kutova
-v1.1 - 2021
-*/
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
@@ -18,7 +5,6 @@ import aed3.ArvoreBMais;
 
 public class Main {
 
-  // Método principal apenas para testes
   public static void main(String[] args) {
 
     ArvoreBMais<ParIntInt> arvore;
@@ -28,6 +14,7 @@ public class Main {
       File d = new File("dados");
       if (!d.exists())
         d.mkdir();
+
       arvore = new ArvoreBMais<>(ParIntInt.class.getConstructor(), 5, "dados/arvore.db");
 
       int opcao;
@@ -36,11 +23,12 @@ public class Main {
         System.out.println("              MENU");
         System.out.println("-------------------------------");
         System.out.println("1 - Inserir");
-        System.out.println("2 - Buscar");
+        System.out.println("2 - Buscar cursos do usuário");
         System.out.println("3 - Excluir");
         System.out.println("4 - Listar todas");
-        System.out.println("5 - Imprimir");
+        System.out.println("5 - Imprimir árvore");
         System.out.println("0 - Sair");
+
         try {
           opcao = Integer.valueOf(console.nextLine());
         } catch (NumberFormatException e) {
@@ -48,66 +36,85 @@ public class Main {
         }
 
         switch (opcao) {
+
           case 1: {
             System.out.println("\nINCLUSÃO");
-            int n1 = 0, n2 = 0;
+
+            int n1;
+            String nomeCurso;
+
             try {
               System.out.print("ID usuario: ");
               n1 = Integer.valueOf(console.nextLine());
-              System.out.print("ID curso: ");
-              n2 = Integer.valueOf(console.nextLine());
+
+              System.out.print("Nome do curso: ");
+              nomeCurso = console.nextLine();
+
             } catch (Exception e) {
-              System.out.println("Não é um número!");
+              System.out.println("Erro na leitura!");
               break;
             }
-            arvore.create(new ParIntInt(n1, n2));
+
+            arvore.create(new ParIntInt(n1, nomeCurso));
             arvore.print();
           }
             break;
+
           case 2: {
             System.out.println("\nBUSCA");
+
             System.out.print("ID usuario: ");
             int n1 = Integer.valueOf(console.nextLine());
-            // Ao passar o segundoID curso: como -1, ele funciona como um coringa
-            // de acordo com a implementação do método compareTo na classe
-            // ParIntInt
-            ArrayList<ParIntInt> lista = arvore.read(new ParIntInt(n1, -1));
 
-            // System.out.print("Num2: ");
-            // int n2 = Integer.valueOf(console.nextLine());
-            // ArrayList<ParIntInt> lista = arvore.read(new ParIntInt(n1, n2));
-            System.out.print("Resposta: ");
-            for (int i = 0; i < lista.size(); i++)
-              System.out.print(lista.get(i).getNum2() + " ");
+            ArrayList<ParIntInt> lista = arvore.read(null);
+
+            System.out.println("Cursos encontrados:");
+
+            for (ParIntInt p : lista) {
+              if (p.getNum1() == n1)
+                System.out.println(p.getNum2());
+            }
           }
             break;
+
           case 3: {
             System.out.println("\nEXCLUSÃO");
+
             System.out.print("ID usuario: ");
             int n1 = Integer.valueOf(console.nextLine());
-            System.out.print("ID curso: ");
-            int n2 = Integer.valueOf(console.nextLine());
-            arvore.delete(new ParIntInt(n1, n2));
+
+            System.out.print("Nome do curso: ");
+            String nomeCurso = console.nextLine();
+
+            arvore.delete(new ParIntInt(n1, nomeCurso));
             arvore.print();
           }
             break;
+
           case 4: {
             System.out.println("\nLISTA COMPLETA");
+
             ArrayList<ParIntInt> lista = arvore.read(null);
+
             for (int i = 0; i < lista.size(); i++)
-              System.out.print(lista.get(i).getNum2() + " ");
+              System.out.println(lista.get(i));
           }
             break;
+
           case 5: {
             arvore.print();
           }
             break;
+
           case 0:
             break;
+
           default:
             System.out.println("Opção inválida");
         }
+
       } while (opcao != 0);
+
       console.close();
 
     } catch (Exception e) {
--- ArvoreBMais/ParIntInt.java
+++ ArvoreBMais/ParIntInt.java
@@ -1,50 +1,43 @@
-/*
-Esta classe representa um objeto para uma entidade
-que será armazenado em uma árvore B+
-
-Neste caso em particular, este objeto é representado
-por dois números inteiros para que possa conter
-relacionamentos entre dois IDs de entidades quaisquer
- 
-Implementado pelo Prof. Marcos Kutova
-v1.0 - 2021
-*/
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class ParIntInt implements aed3.RegistroArvoreBMais<ParIntInt> {
 
   private int num1;
-  private int num2;
-  private short TAMANHO = 8;
+  private String num2;
+
+  private final short TAMANHO = 84;
+  // 4 bytes do int + 80 bytes da string
 
   public ParIntInt() {
-    this(-1, -1);
+    this(-1, "");
   }
 
-  public ParIntInt(int n1) {
-    this(n1, -1);
+public ParIntInt(int n1) {
+  this.num1 = n1;
+  this.num2 = "";
+}
+
+  public ParIntInt(int n1, String n2) {
+    this.num1 = n1;
+    this.num2 = ajustarString(n2);
   }
 
-  public ParIntInt(int n1, int n2) {
-    try {
-      this.num1 = n1; // ID do Usuário
-      this.num2 = n2; // ID da Pergunta
-    } catch (Exception ec) {
-      ec.printStackTrace();
-    }
+  private String ajustarString(String s) {
+    if (s.length() > 40)
+      return s.substring(0, 40);
+
+    while (s.length() < 40)
+      s += " ";
+
+    return s;
   }
 
-   public int getNum1() {
+  public int getNum1() {
     return num1;
   }
 
-  public int getNum2() {
-    return num2;
+  public String getNum2() {
+    return num2.trim();
   }
 
   @Override
@@ -53,35 +46,52 @@ public class ParIntInt implements aed3.RegistroArvoreBMais<ParIntInt> {
   }
 
   public short size() {
-    return this.TAMANHO;
+    return TAMANHO;
   }
 
   public int compareTo(ParIntInt a) {
+
     if (this.num1 != a.num1)
       return this.num1 - a.num1;
-    else
-      // Só compara os valores de Num2, se o Num2 da busca for diferente de -1
-      // Isso é necessário para que seja possível a busca de lista
-      return this.num2 == -1 ? 0 : this.num2 - a.num2;
+
+    // coringa para buscar todos os cursos do usuário
+    if (a.num2.trim().length() == 0)
+      return 0;
+
+    return this.num2.trim().compareTo(a.num2.trim());
   }
 
   public String toString() {
-    return String.format("%3d", this.num1) + ";" + String.format("%-3d", this.num2);
+    return num1 + ";" + num2.trim();
   }
 
   public byte[] toByteArray() throws IOException {
+
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     DataOutputStream dos = new DataOutputStream(baos);
-    dos.writeInt(this.num1);
-    dos.writeInt(this.num2);
+
+    dos.writeInt(num1);
+
+    String s = ajustarString(num2);
+    for (int i = 0; i < 40; i++) {
+      dos.writeChar(s.charAt(i));
+    }
+
     return baos.toByteArray();
   }
 
   public void fromByteArray(byte[] ba) throws IOException {
+
     ByteArrayInputStream bais = new ByteArrayInputStream(ba);
     DataInputStream dis = new DataInputStream(bais);
-    this.num1 = dis.readInt();
-    this.num2 = dis.readInt();
-  }
 
+    num1 = dis.readInt();
+
+    char[] c = new char[40];
+    for (int i = 0; i < 40; i++) {
+      c[i] = dis.readChar();
+    }
+
+    num2 = new String(c);
+  }
 }
