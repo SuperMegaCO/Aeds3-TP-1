8183f6fd0c3178092c545ce482bc59a4adcfd1e8
leozin-amaral18 <127793131+leozin-amaral18@users.noreply.github.com>
2026-04-13 13:31:39 -0300

    Add files via upload

---
+++ CRUD/CRUD_Curso/ParCodigoID.java
@@ -0,0 +1,49 @@
+package CRUD_Curso;
+
+import aed3.*;
+
+public class ParCodigoID implements RegistroHashExtensivel<ParCodigoID> {
+
+    private String codigo;
+    private int id;
+
+    public ParCodigoID() {
+        this("", -1);
+    }
+
+    public ParCodigoID(String codigo, int id) {
+        this.codigo = codigo;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int hash(String codigo) {
+        return codigo.hashCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo.hashCode();
+    }
+
+    @Override
+    public short size() {
+        return 50; // tamanho fixo (ajustável)
+    }
+
+    @Override
+    public byte[] toByteArray() throws Exception {
+        return (codigo + ";" + id).getBytes();
+    }
+
+    @Override
+    public void fromByteArray(byte[] ba) throws Exception {
+        String s = new String(ba);
+        String[] partes = s.split(";");
+        codigo = partes[0];
+        id = Integer.parseInt(partes[1]);
+    }
+}
